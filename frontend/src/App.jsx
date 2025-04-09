25f3b01448af5c37c9370d64498928baf32d3514
sudenazcivelek <sudenazzcivelek@gmail.com>
2025-04-09 21:27:19 +0300

    Sude branchindeki güncellemeler

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <>
       <div>
+      <Header />
         <h1> Hello World </h1>
       </div>
     </>
