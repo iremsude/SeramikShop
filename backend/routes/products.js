bbf810c69f38e92d8e8871aeeba4db117eda5c78
sudenazcivelek <sudenazzcivelek@gmail.com>
2025-06-19 01:18:23 +0300

    Yeni ürünlerin hepsini getirme

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -15,6 +15,18 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Tüm ürünleri getirme (Read - All)
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
 
 
 module.exports = router;
