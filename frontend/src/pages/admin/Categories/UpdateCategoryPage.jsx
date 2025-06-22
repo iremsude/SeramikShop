08fff6b692919e5ebe85860cc5b89b1a850f67ce
iremsude <115478737+iremsude@users.noreply.github.com>
2025-06-22 06:24:24 +0300

    update kategori düzeltme

--- frontend/src/pages/admin/Categories/UpdateCategoryPage.jsx
+++ frontend/src/pages/admin/Categories/UpdateCategoryPage.jsx
@@ -1,51 +1,54 @@
-import { Button, Form, Input, InputNumber, Select, Spin, message } from "antd";
+import { Button, Form, Input, Spin, message } from "antd";
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import { useParams } from "react-router-dom";
 
 const UpdateCategoryPage = () => {
-  const [loading, setLoading] = useState(false);
-  const [categories, setCategories] = useState([]);
-  const navigate = useNavigate();
+const [loading, setLoading] = useState(false);
+const [form] = Form.useForm();
+const params = useParams();
+const categoryId = params.id;
+const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
-  const [form] = Form.useForm();
-  const apiUrl = import.meta.env.VITE_API_BASE_URL;
-  const params = useParams();
-  const productId = params.id;
+  const onFinish = async (values) => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${apiUrl}/api/categories/${categoryId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
 
-  useEffect(() => {
-    const fetchData = async () => {
+      if (response.ok) {
+        message.success("Kategori başarıyla güncellendi.");
+      } else {
+        message.error("Kategori güncellenirken bir hata oluştu.");
+      }
+    } catch (error) {
+      console.log("Kategori güncelleme hatası:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+   useEffect(() => {
+    const fetchSingleCategory = async () => {
       setLoading(true);
 
       try {
-        const [categoriesResponse, singleProductResponse] = await Promise.all([
-          fetch(`${apiUrl}/api/categories`),
-          fetch(`${apiUrl}/api/products/${productId}`),
-        ]);
+        const response = await fetch(`${apiUrl}/api/categories/${categoryId}`);
 
-        if (!categoriesResponse.ok || !singleProductResponse.ok) {
-          message.error("Veri getirme başarısız.");
-          return;
+        if (!response.ok) {
+          throw new Error("Verileri getirme hatası");
         }
 
-        const [categoriesData, singleProductData] = await Promise.all([
-          categoriesResponse.json(),
-          singleProductResponse.json(),
-        ]);
+        const data = await response.json();
 
-        setCategories(categoriesData);
-
-        if (singleProductData) {
+        if (data) {
           form.setFieldsValue({
-            name: singleProductData.name,
-            current: singleProductData.price.current,
-            discount: singleProductData.price.discount,
-            description: singleProductData.description,
-            img: singleProductData.img.join("\n"),
-            colors: singleProductData.colors.join("\n"),
-            sizes: singleProductData.sizes.join("\n"),
-            category: singleProductData.category,
+            name: data.name,
+            img: data.img,
           });
         }
       } catch (error) {
@@ -54,168 +57,47 @@ const UpdateCategoryPage = () => {
         setLoading(false);
       }
     };
-    fetchData();
-  }, [apiUrl, productId, form]);
-
-  const onFinish = async (values) => {
-    console.log(values);
-    const imgLinks = values.img.split("\n").map((link) => link.trim());
-    const colors = values.colors.split("\n").map((link) => link.trim());
-    const sizes = values.sizes.split("\n").map((link) => link.trim());
-    setLoading(true);
-    try {
-      const response = await fetch(`${apiUrl}/api/products/${productId}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          ...values,
-          price: {
-            current: values.current,
-            discount: values.discount,
-          },
-          colors,
-          sizes,
-          img: imgLinks,
-        }),
-      });
-
-      if (response.ok) {
-        message.success("Ürün başarıyla güncellendi.");
-        navigate("/admin/products");
-      } else {
-        message.error("Ürün güncellenirken bir hata oluştu.");
-      }
-    } catch (error) {
-      console.log("Ürün güncelleme hatası:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+    fetchSingleCategory();
+  }, [apiUrl, categoryId, form]);
 
   return (
+
     <Spin spinning={loading}>
-      <Form name="basic" layout="vertical" onFinish={onFinish} form={form}>
-        <Form.Item
-          label="Ürün İsmi"
+      <Form
+        form={form}
+        name="basic"
+        layout="vertical"
+        autoComplete="off"
+        onFinish={onFinish}
+      >
+       <Form.Item
+          label="Kategori İsmi"
           name="name"
           rules={[
             {
               required: true,
-              message: "Lütfen Ürün adını girin!",
+              message: "Lütfen kategori adını girin!",
+            },
+          ]}
+        >
+          <Input />
+        
+        </Form.Item>
+
+        <Form.Item
+          label="Kategori Görseli (Link)"
+          name="img"
+          rules={[
+            {
+              required: true,
+              message: "Lütfen kategori görsel linkini girin!",
             },
           ]}
         >
           <Input />
         </Form.Item>
-        <Form.Item
-          label="Ürün Kategorisi"
-          name="category"
-          rules={[
-            {
-              required: true,
-              message: "Lütfen 1 kategori seçin!",
-            },
-          ]}
-        >
-          <Select>
-            {categories.map((category) => (
-              <Select.Option value={category._id} key={category._id}>
-                {category.name}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
-        <Form.Item
-          label="Fiyat"
-          name="current"
-          rules={[
-            {
-              required: true,
-              message: "Lütfen ürün fiyatını girin!",
-            },
-          ]}
-        >
-          <InputNumber />
-        </Form.Item>
-        <Form.Item
-          label="İndirim Oranı"
-          name="discount"
-          rules={[
-            {
-              required: true,
-              message: "Lütfen bir ürün indirim oranı girin!",
-            },
-          ]}
-        >
-          <InputNumber />
-        </Form.Item>
-        <Form.Item
-          label="Ürün Açıklaması"
-          name="description"
-          rules={[
-            {
-              required: true,
-              message: "Lütfen bir ürün açıklaması girin!",
-            },
-          ]}
-        >
-          <ReactQuill
-            theme="snow"
-            style={{
-              backgroundColor: "white",
-            }}
-          />
-        </Form.Item>
-        <Form.Item
-          label="Ürün Görselleri (Linkler)"
-          name="img"
-          rules={[
-            {
-              required: true,
-              message: "Lütfen en az 4 ürün görsel linki girin!",
-            },
-          ]}
-        >
-          <Input.TextArea
-            placeholder="Her bir görsel linkini yeni bir satıra yazın."
-            autoSize={{ minRows: 4 }}
-          />
-        </Form.Item>
-        <Form.Item
-          label="Ürün Renkleri (RGB Kodları)"
-          name="colors"
-          rules={[
-            {
-              required: true,
-              message: "Lütfen en az 1 ürün rengi girin!",
-            },
-          ]}
-        >
-          <Input.TextArea
-            placeholder="Her bir RGB kodunu yeni bir satıra yazın."
-            autoSize={{ minRows: 4 }}
-          />
-        </Form.Item>
-        <Form.Item
-          label="Ürün Bedenleri"
-          name="sizes"
-          rules={[
-            {
-              required: true,
-              message: "Lütfen en az 1 ürün beden ölçüsü girin!",
-            },
-          ]}
-        >
-          <Input.TextArea
-            placeholder="Her bir beden ölçüsünü yeni bir satıra yazın."
-            autoSize={{ minRows: 4 }}
-          />
-        </Form.Item>
-
-        <Button type="primary" htmlType="submit">
-          Update
+  <Button type="primary" htmlType="submit">
+          Güncelle
         </Button>
       </Form>
     </Spin>
