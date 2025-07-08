bb75469db941c896d6cdc13cd9921bf44eec402b
iremsude <115478737+iremsude@users.noreply.github.com>
2025-07-09 01:36:00 +0300

    use dynamic port for Render

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mainRoute = require("./routes/index.js");
 const app = express();
 const cors = require("cors");
 const logger = require("morgan");
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 dotenv.config();
 
